bf41715f49f982f1a7468ff87d909832db8aded1
Toktobekova-Nora-dev <nuraiymtoktobekkyzy507@gmail.com>
2025-07-17 15:47:26 +0600

    s

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,11 +13,6 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export const metadata = {
-  title: "My Portfolio",
-  description: "Welcome to my portfolio site",
-};
-
 export default function RootLayout({
   children,
 }: Readonly<{
